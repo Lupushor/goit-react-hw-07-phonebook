7c112a1bb4a072f2a3fa502985de26e2ec314f3e
Aleksandr Lupu <103314959+Lupushor@users.noreply.github.com>
2023-06-17 13:48:17 +0200

    HW7

--- src/index.js
+++ src/index.js
@@ -9,10 +9,7 @@ import { store } from 'redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-     
         <App />
- 
     </Provider>
-    ,
   </React.StrictMode>
 );
