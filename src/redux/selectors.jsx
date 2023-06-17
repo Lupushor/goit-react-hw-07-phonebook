79811c219dfff0f2d503f2257bc1e76e1148fdcb
Aleksandr Lupu <103314959+Lupushor@users.noreply.github.com>
2023-06-17 20:21:30 +0200

    HW7

--- src/redux/selectors.jsx
+++ src/redux/selectors.jsx
@@ -1,18 +1,4 @@
-// export const getContacts = ({ contacts: { items } }) => items;
-// export const getFilter = ({ filter }) => filter;
-
 import { createSelector } from "@reduxjs/toolkit";
-
-// export const getFilterContacts = ({ contacts: { items }, filter }) => {
-//   if (!filter) {
-//     return items;
-//   }
-//   return items.filter(
-//     ({ name, number }) =>
-//       name.toLowerCase().includes(filter.toLowerCase()) ||
-//       number.includes(filter)
-//   );
-// };
 
 export const selectContacts = state => state.contacts.items;
 export const selectFilter = state => state.filter;
